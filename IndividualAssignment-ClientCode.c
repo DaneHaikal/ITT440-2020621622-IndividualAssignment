750af9a38e0f267988715421d640b381e829c127
Daniel Haikal <2020621622@student.uitm.edu.my>
2022-05-29 14:43:40 +0000

    Finishing code for client

--- IndividualAssignment-ClientCode.c
+++ IndividualAssignment-ClientCode.c
@@ -27,7 +27,7 @@ int main(int argc, int sockfd, char *argv[])
 	}
 	else
 	{
-		printf("Socket successfully created");
+		printf("Socket successfully created\n");
 	}
 
 	server.sin_addr.s_addr = inet_addr("192.168.56.103");
@@ -39,17 +39,19 @@ int main(int argc, int sockfd, char *argv[])
 		printf("Connection Error");
 		return 1;
 	}
-	
-	printf("Socket connecting to remote server...");
-	printf("Socket Connected");
-	printf("\nARPA Host Name Server Protocol Client\n");
+
+	puts("Socket connecting to remote server...");
+
+	puts("Socket Connected");
+
+	puts("\nARPA Host Name Server Protocol Client\n");
 
 	do
 	{
 		bzero(buff, sizeof(buff));
 		printf("Enter your request: \n");
 		n = 0;
-		
+
 		while ((buff[n++] = getchar()) != '\n');
 			write(sockfd, buff, sizeof(buff));
 			bzero(buff, sizeof(buff));
@@ -57,3 +59,37 @@ int main(int argc, int sockfd, char *argv[])
 	}
 	while (0);
 
+	if(recv(sockDesc, server_reply, 2000, 0) <0)
+	{
+		puts("recv failed");
+	}
+
+	puts("\nReply Received\n");
+
+	puts(server_reply);
+
+	char *hostname = "www.facebook.com";
+	char ipaddr[100];
+	struct hostent *host;
+	struct in_addr  **addr_list;
+	int i;
+
+	if((host = gethostbyname(hostname)) == NULL)
+	{
+		herror("gethostbyname");
+		return 1;
+	}
+	puts("Hostname: www.facebook.com");
+
+	addr_list = (struct in_addr **) host->h_addr_list;
+
+	for(i = 0; addr_list[i] != NULL; i++)
+	{
+		strcpy(ipaddr, inet_ntoa(*addr_list[i]));
+	}
+
+	printf("IP ADDRESS: %s \n",ipaddr);
+	return 0;
+
+	close(sockfd);
+}
